23656bee584135f7aaf20e3efe65d1efac3726ff
Zhan <zhan.enev6@gmail.com>
2023-09-10 17:30:48 +0300

    little progress by Zhan

--- Site-CV/scripts/slider-btns.js
+++ Site-CV/scripts/slider-btns.js
@@ -1,21 +1,21 @@
-let btn =document.querySelector(".btn");
-let slide = document.querySelector("#slide");
+// let btn = document.getElementsByClassName("btn");
+// let slide = document.getElementById("slide");
 
-btn[0].onclick = function () {
-    slide.style.transform = "translateX(0px)"
-}
-btn[1].onclick = function () {
-    slide.style.transform = "translateX(-800px)"
-}
-btn[2].onclick = function () {
-    slide.style.transform = "translateX(-1600px)"
-}
-btn[3].onclick = function () {
-    slide.style.transform = "translateX(-2400px)"
-}
-btn[4].onclick = function () {
-    slide.style.transform = "translateX(-4800px)"
-}
-btn[5].onclick = function () {
-    slide.style.transform = "translateX(-9600px)"
-}
+// btn[0].onclick = function () {
+//   slide.style.transform = "translateX(0px)";
+// };
+// btn[1].onclick = function () {
+//   slide.style.transform = "translateX(-800px)";
+// };
+// btn[2].onclick = function () {
+//   slide.style.transform = "translateX(-1600px)";
+// };
+// btn[3].onclick = function () {
+//   slide.style.transform = "translateX(-2400px)";
+// };
+// btn[4].onclick = function () {
+//   slide.style.transform = "translateX(-4800px)";
+// };
+// btn[5].onclick = function () {
+//   slide.style.transform = "translateX(-9600px)";
+// };

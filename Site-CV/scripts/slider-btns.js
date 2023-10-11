43e9b7139eb5ecad1cf7d35d6db873a230b3f2d2
Zhan <zhanenev6@gmail.com>
2023-10-11 18:49:02 +0300

    Slider change

--- Site-CV/scripts/slider-btns.js
+++ Site-CV/scripts/slider-btns.js
@@ -1,21 +1,34 @@
 // let btn = document.getElementsByClassName("btn");
 // let slide = document.getElementById("slide");
 
+// btn.onclick = function () {
+//     slide.style.transform = "translateX(0px)";
+//     for (i = 0; i < 6; i++) {
+//         btn[i].classList.remove("active");
+//     }
+//     this.classList.add("active");
+// };
+
+
+
 // btn[0].onclick = function () {
-//   slide.style.transform = "translateX(0px)";
+//     slide.style.transform = "translateX(0px)";
+//     for (i = 0; i < 6; i++) {
+//         btn[i].classList.remove("active");
+//     }
+//     this.classList.add("active");
 // };
 // btn[1].onclick = function () {
-//   slide.style.transform = "translateX(-800px)";
+//     slide.style.transform = "translateX(-893px)";
+//     for (i = 0; i < 6; i++) {
+//         btn[i].classList.remove("active");
+//     }
+//     this.classList.add("active");
 // };
 // btn[2].onclick = function () {
-//   slide.style.transform = "translateX(-1600px)";
-// };
-// btn[3].onclick = function () {
-//   slide.style.transform = "translateX(-2400px)";
-// };
-// btn[4].onclick = function () {
-//   slide.style.transform = "translateX(-4800px)";
-// };
-// btn[5].onclick = function () {
-//   slide.style.transform = "translateX(-9600px)";
+//     slide.style.transform = "translateX(-1786px)";
+//     for (i = 0; i < 6; i++) {
+//         btn[i].classList.remove("active");
+//     }
+//     this.classList.add("active");
 // };
